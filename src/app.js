b92368048d5e553d37e9523cacb650f5a0272487
Diksha Deshmukh <Diksha.Deshmukh@SCT.COM>
2025-05-21 14:20:34 +0530

    Tetsing routes

--- src/app.js
+++ src/app.js
@@ -6,6 +6,26 @@ app.use("/test", (req, resp)=>{
     resp.send("Hello from server!!");
 })
 
+app.get("/user", (req, resp)=>{
+    console.log("Users are fetched!!");
+    resp.send("Fetched users!!");
+});
+
+app.post("/user", (req, resp)=>{
+    console.log("User has been created!!");
+    resp.send("User has been saved!!");
+});
+
+app.delete("/user", (req, resp)=>{
+    console.log("User has been deleted!!");
+    resp.send("User is deleted!!");
+});
+
+app.use("/", (req, resp)=>{
+    resp.send("Server is on default route!!");
+});
+
+
 app.listen(3000, ()=>{
     console.log("Server is listning on port 3000...");
 })
